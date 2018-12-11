82313c58d6a8b0a7dde39b739b4383f1c604b9dc
lxx <853555374@qq.com>
2018-12-11 19:13:05 +0800

    first init -webapi

---
+++ bitcoinexplorer-webapi/src/main/java/io/lxx/bitcoinexplorerwebapi/BitcoinexplorerWebapiApplication.java
@@ -0,0 +1,12 @@
+package io.lxx.bitcoinexplorerwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinexplorerWebapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitcoinexplorerWebapiApplication.class, args);
+    }
+}
